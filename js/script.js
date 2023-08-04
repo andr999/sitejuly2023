1eaa85d6e88e0648c82e2c2b79ec3fcc3454d115
andr999 <andreykuchay2@gmail.com>
2023-08-04 11:14:50 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.up').fadeIn();
+        } else {
+            $('.up').fadeOut();
+        }
+    });
+
+
+$(function(){
+	let data = new Date();
+	$("span.year").html(data.getFullYear());
+	$('.up').click(function () {
+    	 $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+       });
+})
